62657258b199c6910fdf918a1eefce96983dff0c
sdfwefk11 <mknhsad3398@naver.com>
2023-02-11 23:32:33 +0900

    Animation(&:hover,&active)

--- react-study01/src/App.js
+++ react-study01/src/App.js
@@ -1,32 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 const Father = styled.div`
   display: "flex";
 `;
 
-const Btn = styled.button`
-  color: aqua;
-  background-color: pink;
-  border: 0;
-  border-radius: 15px;
+const rotationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input = styled.input.attrs({ require: true })`
-  background-color: pink;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: cyan;
+  display: flex;
+  animation: ${rotationAnimation} 1s linear infinite;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 100px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
+
 function App() {
   return (
-    <>
-      {" "}
-      <Father as="header">
-        <Btn>Log in</Btn>
-        <Btn as="a" href="/">
-          Log in
-        </Btn>
-      </Father>
-      <Father>
-        <Input></Input>
-      </Father>
-    </>
+    <Father>
+      <Box>
+        <span>A</span>
+      </Box>
+    </Father>
   );
 }
 
