378804fd705bac4949f4c410d312b59fa99295c6
sdfwefk11 <mknhsad3398@naver.com>
2023-02-14 23:57:19 +0900

    theme

--- react-study01/src/index.js
+++ react-study01/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+const datkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <ThemeProvider theme={datkTheme}>
+    <App />
+  </ThemeProvider>
   // </React.StrictMode>
 );
