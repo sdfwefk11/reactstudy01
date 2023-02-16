bfecda8a7828813ec7545c056ebc4d068679d9e7
sdfwefk11 <mknhsad3398@naver.com>
2023-02-16 23:55:57 +0900

    typescript

--- react-study01/src/index.tsx
+++ react-study01/src/index.tsx
@@ -12,7 +12,7 @@ const lightTheme = {
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")as HTMLElement);
 root.render(
   // <React.StrictMode>
   <ThemeProvider theme={datkTheme}>
