ac72cabf45138a9d2c718a969dd82918edff4d61
sdfwefk11 <mknhsad3398@naver.com>
2023-02-23 23:52:15 +0900

    Theme

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,28 +1,16 @@
-import { useState } from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={value}
-          type="text"
-          placeholder="user name"
-        ></input>
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello</H1>
+    </Container>
   );
 }
 
