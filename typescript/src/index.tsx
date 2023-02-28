23bdac12ac44e1e5fcd951aa7c5462f094db7e91
sdfwefk11 <mknhsad3398@naver.com>
2023-02-28 22:19:04 +0900

    errorElement

--- typescript/src/index.tsx
+++ typescript/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./screens/Router";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
