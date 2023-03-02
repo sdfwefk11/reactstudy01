30dae2c9c5db02577ddd71e3f18e6612996ad2dc
sdfwefk11 <mknhsad3398@naver.com>
2023-03-02 23:18:03 +0900

    useParams 3:56

--- typescript/src/Router.tsx
+++ typescript/src/Router.tsx
@@ -4,6 +4,7 @@ import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import NotFound from "./screens/NotFound";
+import User from "./screens/users/User";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "users/:userId",
+        element: <User />,
       },
     ],
     errorElement: <NotFound />,
---
+++ typescript/src/screens/db.tsx
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    name: "orion2.5",
+  },
+  {
+    id: 2,
+    name: "qk75",
+  },
+];
