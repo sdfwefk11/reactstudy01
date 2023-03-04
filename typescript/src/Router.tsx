e14d898c993a6413a4ec5a0dd4fbf9524f1387fe
sdfwefk11 <mknhsad3398@naver.com>
2023-03-04 22:07:03 +0900

    Outlet

--- typescript/src/Router.tsx
+++ typescript/src/Router.tsx
@@ -4,6 +4,7 @@ import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import NotFound from "./screens/NotFound";
+import Followers from "./screens/users/Followers";
 import User from "./screens/users/User";
 const router = createBrowserRouter([
   {
@@ -22,6 +23,12 @@ const router = createBrowserRouter([
       {
         path: "users/:userId",
         element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+          },
+        ],
       },
     ],
     errorElement: <NotFound />,
