09d8c17dfcc27d837c6c281478ea959c6195eaf8
sdfwefk11 <mknhsad3398@naver.com>
2023-03-06 23:38:49 +0900

    setup7:06

--- typescript/src/Router.tsx
+++ typescript/src/Router.tsx
@@ -1,37 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import ErrorComponent from "./components/ErrorComponent";
-import Root from "./Root";
-import About from "./screens/About";
-import Home from "./screens/Home";
-import NotFound from "./screens/NotFound";
-import Followers from "./screens/users/Followers";
-import User from "./screens/users/User";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        errorElement: <ErrorComponent />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "users/:userId",
-        element: <User />,
-        children: [
-          {
-            path: "followers",
-            element: <Followers />,
-          },
-        ],
-      },
-    ],
-    errorElement: <NotFound />,
-  },
-]);
-export default router;
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+export default Router;
--- typescript/src/index.tsx
+++ typescript/src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import { ThemeProvider } from "styled-components";
+import { Theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <ThemeProvider theme={Theme}></ThemeProvider>
   </React.StrictMode>
 );
