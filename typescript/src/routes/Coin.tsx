a765841d2ff6a62c5527aebb67f1b427cfa15fc6
sdfwefk11 <mknhsad3398@naver.com>
2023-03-14 00:14:14 +0900

    Route States

--- typescript/src/routes/Coin.tsx
+++ typescript/src/routes/Coin.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-
+import styled from "styled-components";
 interface RouteParams {
   coinId: string;
 }
-
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  color: ${(props) => props.theme.accentColor};
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 const Coin = () => {
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <Container>
+      <Header>
+        <Title>{coinId}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 };
 
 export default Coin;
